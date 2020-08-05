d5131b61c69560f0e1e6fa06aa0914d630a217bf
macas5 <mkalinauskas1993@gmail.com>
2020-08-05 11:10:00 -0700

    Added redux to the site and cleaned up a bit

--- robofriends/src/components/CardList.js
+++ robofriends/src/components/CardList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from './Card.js';
 
 const CardList = ({ robots }) => {
-
         return (
         <div>
             { robots.map((user, i) => {
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './App';
+import App from './containers/App';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
